57c5cf6bd3ba0695e72132bfdff32aeae90f7b99
Matheus <matnascimento20@gmail.com>
2023-08-08 09:25:43 -0300

    adicionando banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,16 +1,25 @@
 import "./styles.css"
-import banner1 from '../../assets/images/banner-1.png'
 import banner2 from '../../assets/images/banner-2.png'
 
 
 export default function Banner(){
     return(
         <main>
-            <section>
-                <h4>Bem Vindo</h4>
-                <h1>Participe da nossa pesquisa 
-                    <br/>Cadastre um bichinho! 
-                </h1>
+            <section className="cont">
+                <ul>
+                    <li className="title1">
+                    BEM VINDO!
+                   </li>
+                    <li className="title2">
+                    <span className="azulao">Participe</span> da nossa <span className="laranjao">pesquisa,</span> <br/> 
+                    <span className="laranjao">Cadastre</span> um <span className="azulao">bichinho!</span> 
+                    </li>
+                    <li className="title3">
+                    Responda ao nosso formulário de censo para a contribuição da coleta de dados 
+                    sobre a situação dos cães e gatos da cidade de Ribeirão Pires 
+                    </li>
+                </ul>
+                <img src={banner2} alt="banner" className="foto" />
             </section>
         </main>
     )
