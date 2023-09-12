a8140f89b0f89f85620633bede415bcd5aaeaaa4
Mateus Alves Moraes <mateus890alves@gmail.com>
2023-09-12 09:53:08 -0300

    add

--- server/controller/cadastro.ts
+++ server/controller/cadastro.ts
@@ -1,12 +1,22 @@
-import Express, { request }  from "express";
 import { Request, Response } from "express";
+const mysql = require('mysql')
+
 export const cadastro = async (request:Request, response: Response) =>{
 
-    try {
-        console.log("Rota de Cadastro funcionando");
-        return response.sendStatus(200).end()
-    } catch (error) {
-        console.log("Algum erro aconteceu")
-        return response.sendStatus(400)
-    }
+    // .post("/register", (request: Request, response: Response) => {
+
+    const valores = request.body.data;
+
+    mysql.query("INSERT INTO usuario (nome, rg, cpf, email, senha, cep, cidade, bairro, endereco, numero, complemento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ", 
+        Object.values(valores),
+        (error: any, result: any) => {
+            if (error) {
+                console.log(error);
+                
+            } else {
+                response.send(result);
+            }
+        })
+    // });
+
 } 
--- server/rotas/cadastro.ts
+++ server/rotas/cadastro.ts
@@ -3,5 +3,5 @@ import Express  from "express";
 import { cadastro } from "../controller/cadastro";
 
 export default (router: Express.Router) =>{
-    router.get('/cadastro', cadastro)
+    router.post('/cadastro', cadastro)
 }
